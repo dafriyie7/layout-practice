22809cc3e7d4709f5211bbefd65f3ddd854105a9
Daniel Afriyie <dafriyie7@gmail.com>
2025-08-11 19:16:53 +0000

    minute change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,22 @@
 const Navbar = () => {
   return (
 		<nav className="flex items-center justify-between bg-white w-full pb-3 pt-6 px-10">
-		  <p className="text-3xl font-bold">Logo</p>
-		  
-		  <ul className="flex gap-5 font-semibold">
-			  <li>Item</li>
-			  <li>Item</li>
-			  <li>Item</li>
-			  <li>Item</li>
-			  <li>Item</li>
-		  </ul>
+			<p className="text-3xl font-bold">Logo</p>
 
-		  <div className="flex gap-3 items-center">
-			  <p className="font-semibold">Info</p>
-			  <button className="font-semibold bg-gray-800 text-white px-4 py-1 rounded-full">Button</button>
-		  </div>
+			<ul className="flex gap-5 font-semibold">
+				<li className="hover:text-lg">Item</li>
+				<li className="hover:text-lg">Item</li>
+				<li className="hover:text-lg">Item</li>
+				<li className="hover:text-lg">Item</li>
+				<li className="hover:text-lg">Item</li>
+			</ul>
+
+			<div className="flex gap-3 items-center">
+				<p className="font-semibold">Info</p>
+				<button className="font-semibold bg-gray-800 text-white px-4 py-1 rounded-full">
+					Button
+				</button>
+			</div>
 		</nav>
   );
 }
